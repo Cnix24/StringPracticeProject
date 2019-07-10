a3c9c24bebd0b1b08b5c2820b98d3dd916884b46
Cnix24 <calvinattl24@gmail.com>
2019-07-10 13:08:57 -0600

    Step 97

---
+++ StringsPractice/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace StringsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Calvin";
+            string faveColor = "blue";
+            string cityOfBirth = "Ukiah";
+            string introduction = "Hello, my name is " + name + ", my favorite color is " + faveColor + " and I was born in " + cityOfBirth;
+            Console.WriteLine(introduction);
+
+
+            string someWords = "Here is a sentence.";
+            string someWordsButAngry = someWords.ToUpper();
+            Console.WriteLine(someWordsButAngry);
+
+            StringBuilder s = new StringBuilder("There's five sentences in a paragraph right? That's what they taught us in 6th grade?");
+            s.Append("First sentence. ");
+            s.Append("Second sentence. ");
+            s.Append("Third sentence. ");
+            s.Append("Fourth sentence. ");
+            s.Append("Fifth sentence. ");
+            s.Append("Give me my pulitzer.");
+            Console.WriteLine(s);
+        }
+    }
+}
